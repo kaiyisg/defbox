14f16e23ea5dad8e15f07701e355b91906cd4c22
Alex Leow Yijin <leowyijin@gmail.com>
2017-09-24 07:16:14 -0700

    styling for tripslist

---
+++ Android/app/src/main/java/com/defbox/defbox/TripsListActivity.java
@@ -0,0 +1,13 @@
+package com.defbox.defbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips_list);
+    }
+}
