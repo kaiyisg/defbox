68f8ef81ad27fea0cd4eda27d5b1bdc6a57cfa82
Kai Yi Lee <kaiyisg@yahoo.com.sg>
2017-09-24 08:59:27 -0700

    Add styling

--- Android/app/src/main/java/com/defbox/defbox/CurrentDeliveryActivity.java
+++ Android/app/src/main/java/com/defbox/defbox/CurrentDeliveryActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,12 +19,17 @@ public class CurrentDeliveryActivity extends AppCompatActivity implements Status
     private Activity act;
     private TextView addressText;
     private TextView titleText;
+    private ConstraintLayout state_1;
+    private ConstraintLayout state_2;
+    private ConstraintLayout state_3;
+    private ConstraintLayout state_4;
+    private ConstraintLayout state_5;
 
-    public static final String STATUS_1_TITLE = "Your Package is on the way!";
-    public static final String STATUS_2_TITLE = "Your package has arrived";
-    public static final String STATUS_3_TITLE = "DefBox is opened";
-    public static final String STATUS_4_TITLE = "DefBox is closed";
-    public static final String STATUS_5_TITLE = "DefBox is now locked";
+//    public static final String STATUS_1_TITLE = "Your Package is on the way!";
+//    public static final String STATUS_2_TITLE = "Your package has arrived";
+//    public static final String STATUS_3_TITLE = "DefBox is opened";
+//    public static final String STATUS_4_TITLE = "DefBox is closed";
+//    public static final String STATUS_5_TITLE = "DefBox is now locked";
 
 
     public static final String STATUS_1_ADDRESS = "Palo Alto";
@@ -38,7 +44,11 @@ public class CurrentDeliveryActivity extends AppCompatActivity implements Status
         setContentView(R.layout.activity_current_delivery);
 
         addressText = findViewById(R.id.textView19);
-        titleText = findViewById(R.id.txt_currentDeliveryHeader);
+        state_1.findViewById(R.id.row_1);
+        state_2.findViewById(R.id.row_2);
+        state_3.findViewById(R.id.row_3);
+        state_4.findViewById(R.id.row_4);
+        state_5.findViewById(R.id.row_5);
 
 
         context = getApplicationContext();
@@ -61,23 +71,18 @@ public class CurrentDeliveryActivity extends AppCompatActivity implements Status
         if (status.equals(Back4app.STATUS_0_PRESTART)) {
             Log.d("dd", "onStatus: bad state: "+ status);
         } else if (status.equals(Back4app.STATUS_1_STARTED)) {
-            addressText.setText(STATUS_1_TITLE);
             titleText.setText(STATUS_1_ADDRESS);
             Log.d("dd", "onStatus: no state change: "+ status);
         } else if (status.equals(Back4app.STATUS_2_REACHED)) {
-            addressText.setText(STATUS_2_TITLE);
             titleText.setText(STATUS_2_ADDRESS);
             Log.d("dd", "onStatus: change "+ status);
         } else if (status.equals(Back4app.STATUS_3_OPENED)) {
-            addressText.setText(STATUS_3_TITLE);
             titleText.setText(STATUS_3_ADDRESS);
             Log.d("dd", "onStatus: change: "+ status);
         } else if (status.equals(Back4app.STATUS_4_CLOSED)) {
-            addressText.setText(STATUS_4_TITLE);
             titleText.setText(STATUS_4_ADDRESS);
             Log.d("dd", "onStatus: change: "+ status);
         } else if (status.equals(Back4app.STATUS_5_LOCKED)) {
-            addressText.setText(STATUS_5_TITLE);
             titleText.setText(STATUS_5_ADDRESS);
             Log.d("dd", "onStatus: change: "+ status);
         } else {
