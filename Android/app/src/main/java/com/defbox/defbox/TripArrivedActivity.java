ecea905645b133a52cf75a7cac0af1b0a5fc9355
Alex Leow Yijin <leowyijin@gmail.com>
2017-09-24 08:06:05 -0700

    styling trip arrived screen

---
+++ Android/app/src/main/java/com/defbox/defbox/TripArrivedActivity.java
@@ -0,0 +1,13 @@
+package com.defbox.defbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripArrivedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_arrived);
+    }
+}
